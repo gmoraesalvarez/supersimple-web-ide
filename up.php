e2f74fe1db36bf4a95879f0d6f2783fd04990b8a
Guilherme de Moraes Alvarez <moraes.alvarez@gmail.com>
2014-09-24 17:11:23 -0300

    Create up.php

---
+++ up.php
@@ -0,0 +1,63 @@
+<?php
+    if (isset($_GET['dir'])){$dir = $_GET['dir'];}  else {$dir='../'; }
+    if ($dir=='..') {$dir='../';}
+    if ($dir=='.') {$dir='../';}
+    if ($dir=='./') {$dir='../';}
+    if (isset($_GET['del'])){
+		$del = $_GET['del'];
+		unlink($dir.$del);
+		}  else {$del='nil'; }
+	if (isset($_GET['n'])){$new = $_GET['n'];}  else {$new='nil'; }
+    if (isset($_POST['new_file']) or isset($_POST['new_dir']) ){
+        if ($new=='1') { file_put_contents($dir.'/'.$_POST['new_file'], '/* supersimple-web-ide */'); }
+        if ($new=='2') { mkdir($dir.'/'.$_POST['new_dir']);}
+        }
+    $files1 = glob($dir . '*.{*}', GLOB_BRACE);
+	$dirs = glob($dir . '*', GLOB_ONLYDIR);
+    $alink = '<div><a class="lfile" target="_blank" href="edit.php?doc=';
+    
+?>
+<!doctype html>
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+		<title>supersimple-web-ide</title>
+        <link rel="stylesheet" href="pastas.css">
+	</head>
+	<body>
+		<a href="index.php?dir=<? echo $dir ?>" style="min-height:16px;font-size:32px;border:#555 solid 1px;">↲</a>
+		
+		<div id="pastas">
+
+		<form enctype="multipart/form-data" action="uploader.php?dir=<?php echo $dir ?>" method="POST">
+            <input type="hidden" name="MAX_FILE_SIZE" value="100000" />
+            Upload: <input name="uploadedfile" type="file" />
+            <input type="submit" value="Upload File" />
+        </form>
+
+		<form action="index.php?n=2&dir=<? echo $dir ?>" method="POST">
+                <input type="text" value="new_folder" name="new_dir">
+                <input type="submit" value="create">
+            </form> 
+
+		<form action="index.php?n=1&dir=<? echo $dir ?>" method="POST">
+                <input type="text" value="new_file.php" name="new_file">
+                <input type="submit" value="create">
+           </form>
+		</div>
+		<div id="pastas">
+		    <p><?php echo $dir.'<br>';?></p>
+            <?php
+                foreach($dirs as $key => $value){
+					echo '<div><a class="ldir" href="index.php?dir='.$value.'/"><p>'.basename($value).'</p></a><a href="up.php?dir='.$dir.'&del='.basename($value).'" style="font-size:32px;color:#f53">×</a></div>';}
+            ?>
+        </div>
+		<div id="arquivos">
+			<?php
+				foreach($files1 as $key => $value){
+					echo $alink.$value.'"><p>'.basename($value).'</p></a><a href="up.php?dir='.$dir.'&del='.basename($value).'" style="font-size:32px;color:#f53">×</a></div>';}
+			?>
+		</div>		
+
+	</body>
